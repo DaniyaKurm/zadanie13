e9455281595bc120aa3f1c1205d754752018852b
Your NameDaniya <you@example.com>
2023-12-21 18:33:42 +0300

    Initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,10 @@
 package ru.netology.service;
+import org.junit.Test;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+
     CashbackHackService service = new CashbackHackService();
 
     @Test
